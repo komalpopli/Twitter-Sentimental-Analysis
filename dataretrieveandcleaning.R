401c5163954d3482ccc51bbdf343c08c64567e5f
komal popli <komalpopli100@gmail.com>
2019-04-29 12:00:34 +0530

    Add files via upload
     retrieval and cleaning of twitter data

---
+++ dataretrieveandcleaning.R
@@ -0,0 +1,60 @@
+library(RColorBrewer)
+library(wordcloud)
+library(tm)
+library(twitteR)
+library(ROAuth)
+library(NLP)
+library(plyr)
+library(stringr)
+library(base64enc)
+library(ggplot2)
+library(stringi)
+library(httr)
+library(RCurl)
+library(syuzhet)
+
+
+
+download.file(url="http://curl.haxx.se/ca/cacert.pem",destfile="cacert.pem")
+# Set constant requestURL
+requestURL <- "https://api.twitter.com/oauth/request_token"
+# Set constant accessURL
+accessURL <- "https://api.twitter.com/oauth/access_token"
+# Set constant authURL
+authURL <- "https://api.twitter.com/oauth/authorize"
+
+consumer_key <- 'BMpfmSmQwaizU0nILjsojfgWQ'
+consumer_secret <- 'KAJFPHjd0G0GPoq6Y7groRLSeUyLhIR5nDCtCZZDshFeWxFTvE'
+access_token <- '3073142365-UM1B69AVnaDTccYGBPu0X3rM5ImrQ6BjMswlklN'
+access_secret <- '5y76PTdAYEWhAK73yfnEAMQUJuDFD9GYppo0OYXU0fErQ'
+
+setup_twitter_oauth(consumer_key,
+                    consumer_secret,
+                    access_token,
+                    access_secret) 
+
+tweets_g <- searchTwitter("google", n=100,lang = "en")
+tweets <- twListToDF(tweets_g)
+View(tweets)
+
+tweet <- tweets$text
+tweet <- tolower(tweet)
+tweet <- gsub("rt","",tweet)
+tweet <- gsub("@\\w+","",tweet)
+tweet <- gsub("[[:punct:]]","",tweet)
+tweet <- gsub("http\\w+","",tweet)
+tweet <- gsub("[ |\t]{2,}","",tweet)
+tweet <- gsub("^ ","",tweet)
+tweet <- gsub(" $","",tweet)
+
+#wordcloud(tweet.text.corpus,min.freq = 10,random.color = TRUE,max.words = 500)
+
+tweet_corpus <- Corpus(VectorSource(as.vector(tweet))) 
+tweet_corpus <- tm_map(tweet_corpus, removeWords, stopwords("english"))
+tweet_DTM <- DocumentTermMatrix(tweet_corpus, control = list(wordLengths = c(2, Inf)))
+wordcloud(tweet_corpus,min.freq = 2,colors=brewer.pal(8, "Dark2"),random.color = TRUE,max.words = 500)
+
+
+
+
+
