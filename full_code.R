d7a7f77dcc99c7de94e750ba6c6421ebc5393a02
komal popli <komalpopli100@gmail.com>
2019-05-28 14:09:02 +0530

    Add files via upload

---
+++ full_code.R
@@ -0,0 +1,68 @@
+
+library(twitteR)
+library(ROAuth)
+library(syuzhet)
+consumer_key <- '47jjrxeqsZgHtjLpbHQ2QGpgQ'
+consumer_secret <- 'kkCQveDjFS8wNW9Em45VC51nzuuYAqpX5mIjdhL3i9vo6sUYUD'
+access_token <- '3073142365-a1o4BINz8HeEmA9kWnt66M9W2SfEfOxAUk6icy3'
+access_secret <- 'lCJ0eDsqECORtOnxJpUB5uBkLguy3Wbor7qXXvmq7FXRC'
+
+setup_twitter_oauth(consumer_key,
+                    consumer_secret,
+                    access_token,
+                    access_secret)
+searched_string <- "#Tuesday"
+tweets_g <- searchTwitter(searched_string,n=100,lang = "en")
+tweet <- twListToDF(tweets_g)
+cleaned_data <- lapply(tweet$text, function(tweets.df2){
+  tweets.df2 = gsub('http\\S+\\s*','',tweets.df2)
+  tweets.df2 = gsub('\\b+RT','',tweets.df2)
+  tweets.df2 = gsub('#\\S+','',tweets.df2)
+  tweets.df2 = gsub('@\\S+','',tweets.df2)
+  tweets.df2 = gsub('[[:cntrl:]]', '', tweets.df2)
+  tweets.df2 = gsub('\\d', '', tweets.df2)
+  tweets.df2 = gsub('[[:punct:]]', '', tweets.df2)
+  tweets.df2 = gsub('^[[:space:]]*', '', tweets.df2)
+  tweets.df2 = gsub("[[:space:]]*$","",tweets.df2)
+  tweets.df2 = gsub(' +',' ',tweets.df2)
+})
+cleaned_data<-as.character(cleaned_data)
+word.df <-as.vector((cleaned_data))
+sent.value <- get_sentiment(word.df)
+positive.tweets<-word.df[sent.value>0]
+negative.tweets<-word.df[sent.value<0]
+neutral.tweets<-word.df[sent.value==0]
+category_senti<-ifelse(sent.value<0,"Negative",ifelse(sent.value>0,"Positive","Neutral"))
+s<-data.frame(category_senti,sent.value)
+data <- data.frame(cleaned_data,category_senti)
+tweet_info <- cbind(tweet,data)
+head(tweet_info)
+keeps <- c("cleaned_data", "category_senti","created","screenName","latitude","longitude","id")
+tweet_info <- tweet_info[keeps]
+tweet_info$id_topic  <- 0
+tweet_info
+
+library(RMySQL)
+library(dbConnect)
+# 2. Settings
+db_user <- "root"
+db_password <- "password"
+db_name <- "twitter"
+db_table <- "data"
+db_host <- "127.0.0.1" # for local access
+db_port <- 3306
+mydb <-  dbConnect(MySQL(), user = db_user, password = db_password,
+                   dbname = db_name, host = db_host, port = db_port)
+dbSendQuery(mydb,"INSERT INTO topic_ref (topic_name) VALUES ('#Saturday');")
+dbSendQuery(mydb,"INSERT INTO tweet_info(id_topic) SELECT topic_id FROM topic_ref;")
+dbWriteTable(mydb,  name = 'tweet_info',value = tweet_info, append=T, row.names=F, overwrite=F) 
+
+
+#dbSendQuery(mydb, "INSERT INTO topic_ref
+#(author_last, author_first, country)
+#VALUES('Kumar','Manoj','India');")
+xyz <- dbReadTable(conn = mydb, name = 'tweet_info')
+abc <- dbReadTable(conn = mydb, name = 'topic_ref')
+View(xyz)
+abc
+#}
--- plumber.R
+++ plumber.R
@@ -1,28 +1,35 @@
 # plumber.R
 library(plumber)
 
+library(RMySQL)
+library(dbConnect)
+# 2. Settings
+db_user <- "root"
+db_password <- "password"
+db_name <- "twitter"
+db_table <- "dataf"
+db_host <- "127.0.0.1" # for local access
+db_port <- 3306
 
+# 3. Read data from db
+mydb <-  dbConnect(MySQL(), user = db_user, password = db_password,
+                   dbname = db_name, host = db_host, port = db_port)
 #* get data
 #* @param n The number of rows
 #* @get /list
-function(n){
-  library(RMySQL)
-  library(dbConnect)
-  # 2. Settings
-  db_user <- "root"
-  db_password <- "password"
-  db_name <- "twitter"
-  db_table <- "dataf"
-  db_host <- "127.0.0.1" # for local access
-  db_port <- 3306
-  
-  # 3. Read data from db
-  mydb <-  dbConnect(MySQL(), user = db_user, password = db_password,
-                     dbname = db_name, host = db_host, port = db_port)
-  result <- dbSendQuery(mydb,"select * from rom")
-  df=fetch(result,as.numeric(n))
-  print(df)
+function(){
+  result <- dbSendQuery(mydb,"select * from topic_ref")
+  df=fetch(result)
 }
+
+#* tweet_info
+#* @param b topic_id
+#* @get /
+function(b){
+  result <- dbSendQuery(mydb,"select * from tweet_info where id_topic = 0")
+  df=fetch(result)
+}
+
 
 
 
